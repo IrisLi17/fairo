b50324fd6b3699f04cee52ea6b47610d2b88d8ae
Yunfei Li <yunfei@c19-dev>
2022-10-05 00:04:40 +0800

    can collect demo

--- polymetis/examples/5_collect_demo.py
+++ polymetis/examples/5_collect_demo.py
@@ -1,16 +1,23 @@
+from collections import deque
 from polymetis import RobotInterface, GripperInterface, CameraInterface
 import polymetis_pb2
-import keyboard
+import termios, tty, sys
 import queue
 import threading
 import time
 import torch
+import os
 import pickle
+import matplotlib.pyplot as plt
 
 
 class DemoCollector:
-    VALID_KEYS = ["a", "w", "s", "d", "i", "k", "space", "q"]
-    def __init__(self, ip_address="localhost") -> None:
+    VALID_KEYS = ["a", "w", "s", "d", "i", "k", "space", "esc"]
+    KEY_MAPPINGS = {
+        32: "space",
+        27: "esc"
+    }
+    def __init__(self, ip_address="localhost"):
         self.robot = RobotInterface(
             ip_address=ip_address
         )
@@ -20,94 +27,99 @@ class DemoCollector:
         self.camera = CameraInterface(
             ip_address=ip_address
         )
-        self._keyboard_queue = queue.Queue(maxsize=1)
-        self._robstate_queue = queue.Queue(maxsize=1)
-        self._image_queue = queue.Queue(maxsize=1)
+        self._image_buffer = deque(maxlen=1)
         self._keyboard_thr = threading.Thread(
             target=self._keyboard_listener,
             daemon=True,
         )
-        self._camera_thr = threading.Thread(
-            target=self._camera_listener,
-            daemon=True
-        )
-        self._command_thr = threading.Thread(
-            target=self._command_executor,
-            daemon=True,
-        )
-    
-    def _keyboard_listener(self):
+        
+    def run(self, demo_path="demo.pkl"):
+        if os.path.exists(demo_path):
+            ans = input(demo_path + " exists, going to remove? [y]")
+            if ans == "y":
+                os.remove(demo_path)
+            else:
+                return
+        self.robot.go_home()
+        self.robot_initial_quat = self.robot.get_ee_pose()[1]
+        self.robot.start_cartesian_impedance()
+        self._keyboard_thr.start()
+        # self._command_thr.start()
+        fig, ax = plt.subplots(1, 1)
         print("Listening keyboard events")
-        t0 = time.time()
-        while True:
-            event = keyboard.read_event()
-            if event.event_type == keyboard.KEY_DOWN and event.name in self.VALID_KEYS:
-                self._keyboard_queue.put((event.name, time.time() - t0))
-            if event.name == "q":
-                break
-    
-    def _camera_listener(self):
-        print("Listening camera events")
-        old_timestamp = None
         while True:
             image, timestamp = self.camera.read_once()
-            if timestamp != old_timestamp:
-                self._image_queue.put((image, timestamp))
-                old_timestamp = timestamp
-    
-    def _robstate_listener(self):
-        print("Listening robot states")
+            self._image_buffer.append((image, timestamp))
+            ax.cla()
+            ax.imshow(image)
+            ax.set_title(timestamp)
+            plt.pause(0.01)
+        
+    def _keyboard_listener(self):
         while True:
-            robot_state: polymetis_pb2.RobotState = self.robot.get_robot_state()
-            gripper_state: polymetis_pb2.GripperState = self.gripper.get_state()
-            self._robstate_queue.put((robot_state, gripper_state))
-
-    def _command_executor(self):
-        self.robot.start_cartesian_impedance()
-        while True:
-            action, timestamp = self._keyboard_queue.get()
-            if self._robstate_queue.qsize() > 0 and self._image_queue.qsize() > 0:
-                robot_state, gripper_state = self._robstate_queue.get(block=False)
-                camera_image = self._image_queue.get(block=False)
+            keyname = self._getkey()
+            print(keyname, "pressed")
+            if keyname in self.VALID_KEYS:
+                record_obj = dict()
+                robot_state = self.robot.get_robot_state()
+                gripper_state = self.gripper.get_state()
+                camera_image, timestamp = self._image_buffer.pop()
                 eef_pos, eef_quat = self.robot.robot_model.forward_kinematics(torch.Tensor(robot_state.joint_positions))
-                if action == "a":
+                print("EEF quat", eef_quat)
+                if keyname == "a":
                     eef_pos = eef_pos + torch.Tensor([0.01, 0.0, 0.0])
-                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=eef_quat)
-                elif action == "d":
+                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=self.robot_initial_quat)
+                elif keyname == "d":
                     eef_pos = eef_pos + torch.Tensor([-0.01, 0.0, 0.0])
-                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=eef_quat)
-                elif action == "w":
+                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=self.robot_initial_quat)
+                elif keyname == "w":
                     eef_pos = eef_pos + torch.Tensor([0.0, 0.01, 0.0])
-                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=eef_quat)
-                elif action == "s":
+                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=self.robot_initial_quat)
+                elif keyname == "s":
                     eef_pos = eef_pos + torch.Tensor([0.0, -0.01, 0.0])
-                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=eef_quat)
-                elif action == "i":
+                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=self.robot_initial_quat)
+                elif keyname == "i":
                     eef_pos = eef_pos + torch.Tensor([0.0, 0.0, 0.01])
-                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=eef_quat)
-                elif action == "k":
+                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=self.robot_initial_quat)
+                elif keyname == "k":
                     eef_pos = eef_pos + torch.Tensor([0.0, 0.0, -0.01])
-                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=eef_quat)
-                elif action == "space":
+                    self.robot.update_desired_ee_pose(position=eef_pos, orientation=self.robot_initial_quat)
+                elif keyname == "space":
                     if not gripper_state.is_moving:
                         if gripper_state.is_grasped:
                             self.gripper.goto(0.08, 0.1, 1)
+                            record_obj["desired_gripper"] = "open"
                         else:
                             self.gripper.grasp(0.1, 1)
-                elif action == "q":
+                            record_obj["desired_gripper"] = "close"
+                elif keyname == "esc":
+                    os.system("stty sane")
+                    print("Exit keyboard listener")
                     break
                 # Save
                 with open("demo.pkl", "ab") as f:
-                    record_obj = dict(
+                    record_obj.update(dict(
                         robot_state=robot_state, gripper_state=gripper_state, image=camera_image, 
                         desired_eef_pos=eef_pos, desired_eef_quat=eef_quat
-                    )
+                    ))
                     pickle.dump(record_obj, f)
                     print("Demo saved")
-        self.robot.terminate_current_policy()
+
+    def _getkey(self):
+        old_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+        try:
+            while True:
+                b = os.read(sys.stdin.fileno(), 3).decode()
+                if len(b) == 3:
+                    k = ord(b[2])
+                else:
+                    k = ord(b)
+                return self.KEY_MAPPINGS.get(k, chr(k))
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
 
 if __name__ == "__main__":
     demo_controller = DemoCollector(ip_address="101.6.103.171")
-    
-    
+    demo_controller.run()
