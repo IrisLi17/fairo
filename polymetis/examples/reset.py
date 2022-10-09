f551bd65ec6da1e3028849e9e313ffd53e555edf
Yunfei Li <yunfei@c19-nuc>
2022-10-09 16:46:52 +0800

    Change reset pose to the same as in ros

--- polymetis/examples/reset.py
+++ polymetis/examples/reset.py
@@ -1,4 +1,5 @@
 from polymetis import RobotInterface
+import torch
 
 
 if __name__ == "__main__":
@@ -6,6 +7,9 @@ if __name__ == "__main__":
     robot = RobotInterface(
         ip_address="101.6.103.171",
     )
-
+    # Ready pose in ROS
+    robot.set_home_pose(
+        torch.Tensor([0., -0.785, 0., -2.356, 0., 1.571, 0.785])
+    )
     # Reset
     robot.go_home()
