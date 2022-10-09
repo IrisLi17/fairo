dec9c160ca0b589d3d914cee8dc67e050d2ae122
Yunfei Li <yunfei@c19-dev>
2022-10-09 15:41:41 +0800

    implement handeye calibration, not tested

--- polymetis/examples/calibration/calibration_service.py
+++ polymetis/examples/calibration/calibration_service.py
@@ -1,10 +1,10 @@
 from collections import deque
 import time
 import threading
-from typing import Literal, Tuple
+from typing import List, Literal, Tuple
 import cv2  # opencv-contrib-python
 import numpy as np
-from polymetis import CameraInterface
+from polymetis import CameraInterface, RobotInterface
 import matplotlib.pyplot as plt
 import termios, tty, sys, os
 from copy import deepcopy
@@ -34,6 +34,7 @@ class CalibrationBackend:
         corners = np.array(corners).squeeze(axis=1)
         ids = np.array(ids).flatten()
         if use_intrinsic:
+            print("Warning: use_intrinsic may leads to weird results")
             (ret, charuco_corners, charuco_ids) = cv2.aruco.interpolateCornersCharuco(
                 corners, ids, image, self.board, cameraMatrix=self.camera_matrix, distCoeffs=self.dist_coeffs)
         else:
@@ -41,9 +42,15 @@ class CalibrationBackend:
                 corners, ids, image, self.board)
         if charuco_ids is None or len(charuco_ids) == 0:
             raise RuntimeError("Cannot detect checker board corners")
+        is_valid, board_rvec, board_tvec = cv2.aruco.estimatePoseCharucoBoard(
+            charuco_corners, charuco_ids, self.board, self.camera_matrix, self.dist_coeffs, None, None
+        )
+        if not is_valid:
+            board_rvec = None
+            board_tvec = None
         charuco_corners = np.squeeze(charuco_corners, axis=1)
         charuco_ids = np.squeeze(charuco_ids, axis=1)
-        return corners, ids, charuco_corners, charuco_ids
+        return corners, ids, charuco_corners, charuco_ids, board_rvec, board_tvec
     
     def calibrate_intrinsic(self, list_charuco_corners, list_charuco_ids):
         # marker_corners, marker_ids, charuco_corners, charuco_ids = self.detect(image, use_intrinsic=False)
@@ -63,6 +70,29 @@ class CalibrationBackend:
             )
         return reproj_error, new_cam_matrix, new_dist_coeffs
     
+    def calibrate_handeye(
+        self, list_ee_pose: List[np.ndarray], 
+        list_r_board: List[np.ndarray], list_t_board: List[np.ndarray], eye_on_hand: bool
+    ):
+        # list_ee_pose: list of 4X4 ee pose
+        # list_r_board: list of 3X1 rotation vector
+        # list_t_board: list of 3X1 translation
+        assert len(list_ee_pose) == len(list_r_board) and len(list_r_board) == len(list_t_board)
+        if eye_on_hand:
+            list_r_tcp = [pose[:3, :3] for pose in list_ee_pose]
+            list_t_tcp = [pose[:3, 3:] for pose in list_ee_pose]
+            r_gripper_cam, t_gripper_cam = cv2.calibrateHandEye(list_r_tcp, list_t_tcp, list_r_board, list_t_board)
+            gripper_T_cam = np.concatenate([np.concatenate([r_gripper_cam, t_gripper_cam], axis=-1), np.array([0., 0., 0., 1.])], axis=0)
+            return gripper_T_cam
+        else:
+            # eye on base
+            inv_list_ee_pose = [np.linalg.inv(pose) for pose in list_ee_pose]
+            inv_list_r_tcp = [pose[:3, :3] for pose in inv_list_ee_pose]
+            inv_list_t_tcp = [pose[:3, 3:] for pose in inv_list_ee_pose]
+            r_base_cam, t_base_cam = cv2.calibrateHandEye(inv_list_r_tcp, inv_list_t_tcp, list_r_board, list_t_board)
+            base_T_cam = np.concatenate([np.concatenate([r_base_cam, t_base_cam], axis=-1), np.array([0., 0., 0., 1.])], axis=0)
+            return base_T_cam
+
     def draw_markers(self, image, marker_corners, charuco_corners=None):
         image = image.copy().astype(np.uint8)
         for i in range(len(marker_corners)):
@@ -82,8 +112,14 @@ class CalibrationService:
         32: "space",
         27: "esc"
     }
-    def __init__(self, ip_address) -> None:
+    def __init__(self, ip_address, calibration_type="handeye") -> None:
         self.camera_interface = CameraInterface(ip_address=ip_address)
+        try:
+            self.robot_interface = RobotInterface(ip_address=ip_address)
+        except:
+            print("Warning: robot not connected")
+            assert calibration_type != "handeye"
+            self.robot_interface = None
         camera_intrinsic = self.camera_interface.get_intrinsic()
         camera_matrix = np.array(
             [
@@ -95,8 +131,13 @@ class CalibrationService:
         dist_coeffs = camera_intrinsic["coeffs"]
         image, stamp = self.camera_interface.read_once()
         self.calibration_backend = CalibrationBackend(camera_matrix, dist_coeffs, image.shape[1], image.shape[0])
+        assert calibration_type in ["intrinsic", "handeye"]
+        self.calibration_type = calibration_type
         self.capture_image_lock = False
-        self.current_image_and_feature = None
+        self.current_image_and_feature = dict(
+            image=None, charuco_corners=None, charuco_ids=None, board_rvec=None, board_tvec=None,
+            ee_pose=None,
+        )
         self.captured_data = deque(maxlen=20)
         self.merged_feature_image = np.zeros_like(image, dtype=np.uint8)
         self.calibration_result = None
@@ -113,12 +154,13 @@ class CalibrationService:
                 self.capture_image_lock = True
                 self.captured_data.append(deepcopy(self.current_image_and_feature))
                 # update merged features
-                for i in range(len(self.current_image_and_feature[1])):
-                    point = self.current_image_and_feature[1][i]
-                    self.merged_feature_image[
-                        max(int(point[1]) - 1, 0): min(int(point[1]) + 2, self.merged_feature_image.shape[0]), 
-                        max(int(point[0]) - 1, 0): min(int(point[0]) + 2, self.merged_feature_image.shape[1])
-                    ] = np.array([255, 0, 0], dtype=np.uint8)
+                self.merged_feature_image = np.zeros_like(self.merged_feature_image)
+                for i in range(len(self.captured_data)):
+                    for point in self.captured_data[i]["charuco_corners"]:
+                        self.merged_feature_image[
+                            max(int(point[1]) - 1, 0): min(int(point[1]) + 2, self.merged_feature_image.shape[0]), 
+                            max(int(point[0]) - 1, 0): min(int(point[0]) + 2, self.merged_feature_image.shape[1])
+                        ] = np.array([255, 0, 0], dtype=np.uint8)
                 print(f"Capture {len(self.captured_data)}")
                 self.capture_image_lock = False
             elif pressed_key == "r":
@@ -126,15 +168,26 @@ class CalibrationService:
                     print("Insufficient captures, please collect more")
                     continue
                 self.capture_image_lock = True
-                all_charuco_corners = [self.captured_data[i][1] for i in range(len(self.captured_data))]
-                all_charuco_ids = [self.captured_data[i][2] for i in range(len(self.captured_data))]
-                reproj_error, cam_matrix, dist_coeffs = self.calibration_backend.calibrate_intrinsic(all_charuco_corners, all_charuco_ids)
-                self.calibration_result = {"cam_matrix": cam_matrix, "dist_coeffs": dist_coeffs, "reproj_error": reproj_error}
-                print(f"reproj error: {reproj_error}\ncamera matrix: {cam_matrix}\ndist coeffs: {dist_coeffs}")
+                if self.calibration_type == "intrinsic":
+                    all_charuco_corners = [self.captured_data[i]["charuco_corners"] for i in range(len(self.captured_data))]
+                    all_charuco_ids = [self.captured_data[i]["charuco_ids"] for i in range(len(self.captured_data))]
+                    reproj_error, cam_matrix, dist_coeffs = self.calibration_backend.calibrate_intrinsic(all_charuco_corners, all_charuco_ids)
+                    self.calibration_result = {"cam_matrix": cam_matrix, "dist_coeffs": dist_coeffs, "reproj_error": reproj_error}
+                    print(f"reproj error: {reproj_error}\ncamera matrix: {cam_matrix}\ndist coeffs: {dist_coeffs}")
+                elif self.calibration_type == "handeye":
+                    all_ee_pose = [self.captured_data[i]["ee_pose"] for i in range(len(self.captured_data))]
+                    all_board_rvec = [self.captured_data[i]["board_rvec"] for i in range(len(self.captured_data))]
+                    all_board_tvec = [self.captured_data[i]["board_tvec"] for i in range(len(self.captured_data))]
+                    base_T_cam = self.calibration_backend.calibrate_handeye(all_ee_pose, all_board_rvec, all_board_tvec, eye_on_hand=False)
+                    self.calibration_result = {"base_T_cam": base_T_cam}
+                    print(f"base_T_cam", self.calibration_result)
                 self.capture_image_lock = False
             elif pressed_key == "s":
                 import pickle
-                save_path = "calib_intrinsic.pkl"
+                if self.calibration_type == "intrinsic":
+                    save_path = "calib_intrinsic.pkl"
+                elif self.calibration_type == "handeye":
+                    save_path = "calib_handeye.pkl"
                 with open(save_path, "wb") as f:
                     pickle.dump(self.calibration_result, f)
                 print(f"Calibration result saved to {save_path}")
@@ -144,8 +197,24 @@ class CalibrationService:
         while True:
             if not self.capture_image_lock:
                 image, stamp = self.camera_interface.read_once()
-                marker_corners, marker_ids, charuco_corners, charuco_ids = self.calibration_backend.detect(image)
-                self.current_image_and_feature = (image, charuco_corners, charuco_ids)
+                marker_corners, marker_ids, charuco_corners, charuco_ids, board_rvec, board_tvec = self.calibration_backend.detect(image)
+                # self.current_image_and_feature = (image, charuco_corners, charuco_ids, board_rvec, board_tvec)
+                self.current_image_and_feature.update(
+                  dict(
+                      image=image, charuco_corners=charuco_corners, charuco_ids=charuco_ids,
+                      board_rvec=board_rvec, board_tvec=board_tvec,
+                  )  
+                )
+                if self.robot_interface is not None:
+                    ee_pos, ee_quat = self.robot_interface.get_ee_pose()
+                    import torchcontrol.transform.rotation as rotation
+                    ee_rot_mat = rotation.from_quat(ee_quat).as_matrix()
+                    ee_pose = np.concatenate([np.concatenate([ee_rot_mat.numpy(), ee_pos.numpy().reshape(3, 1)], axis=-1), np.array([0, 0, 0, 1])], axis=0)
+                    self.current_image_and_feature.update(
+                        dict(
+                            ee_pose=ee_pose
+                        )
+                    )
                 new_image = self.calibration_backend.draw_markers(image, marker_corners, charuco_corners)
                 ax[0].cla()
                 ax[0].imshow(new_image)
@@ -172,5 +241,6 @@ class CalibrationService:
         
 
 if __name__ == "__main__":
-    calibrator = CalibrationService("localhost")
+    calibrator = CalibrationService("localhost", calibration_type="intrinsic")
+    # calibrator = CalibrationService("101.6.103.171", calibration_type="handeye")
     calibrator.run()
