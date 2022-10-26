09a83cc96f31eb20bbf03f48510adfd78c169671
Yunfei Li <yunfei@c19-dev>
2022-10-26 15:49:29 +0800

    Decent demo collection

--- polymetis/examples/5_collect_demo.py
+++ polymetis/examples/5_collect_demo.py
@@ -22,7 +22,7 @@ class DemoCollector:
         32: "space",
         27: "esc"
     }
-    def __init__(self, ip_address="localhost"):
+    def __init__(self, ip_address="localhost", save_file=True):
         self.robot = RobotInterface(
             ip_address=ip_address
         )
@@ -33,12 +33,16 @@ class DemoCollector:
             ip_address=ip_address
         )
         
+        self.save_file = save_file
+
         self._image_buffer = deque(maxlen=1)
         self.read_image_lock = False
         self._camera_thr = threading.Thread(
             target=self._camera_listener, daemon=True
         )
 
+        self.start_or_pause = False # True: start, False: pause
+        self.stopped = False
         pygame.init()
         self.joystick = pygame.joystick.Joystick(0)
         self.joystick.init()
@@ -66,15 +70,23 @@ class DemoCollector:
         eef_quat = (rotation.from_quat(torch.Tensor([0, 0, np.sin(np.pi / 8), np.cos(np.pi / 8)])) * \
             rotation.from_quat(torch.Tensor([1.0, 0, 0, 0]))).as_quat()
         print("EEF quat", eef_quat)
+        old_robot_state = (self.robot.get_joint_positions(), self.gripper.get_state().width)
+        file_count = 0
+        raw_data_count = 0
         while True:
+            if self.stopped:
+                break
+            if not self.start_or_pause:
+                time.sleep(0.1)
+                continue
             record_obj = dict()
-            self.read_image_lock = True
+            # self.read_image_lock = True
             try:
                 camera_image, stamp = self._image_buffer.pop()
             except:
-                self.read_image_lock = False
+            #     self.read_image_lock = False
                 continue
-            self.read_image_lock = False
+            # self.read_image_lock = False
             robot_state = self.robot.get_robot_state()
             gripper_state = self.gripper.get_state()
             command = self._command_queue.get()
@@ -86,6 +98,10 @@ class DemoCollector:
                     else:
                         self.gripper.grasp(0.1, 1)
                         record_obj["desired_gripper"] = "close"
+                    # wait for finger
+                    time.sleep(0.5)
+                    while self.gripper.get_state().is_moving:
+                        time.sleep(0.5)
                 else:
                     # skip this command
                     self._command_queue.task_done()
@@ -101,66 +117,84 @@ class DemoCollector:
                     self.robot.update_desired_ee_pose(eef_pos, eef_quat)
             else:
                 raise NotImplementedError
-            time.sleep(0.5)
+            # time.sleep(0.5)
             self._command_queue.task_done()
-            with open("demo.pkl", "ab") as f:
-                record_obj.update(dict(
-                    robot_state=robot_state, gripper_state=gripper_state, image=camera_image.astype(np.uint8), 
-                    desired_eef_pos=eef_pos, desired_eef_quat=eef_quat, image_timestamp=stamp,
-                ))
-                pickle.dump(record_obj, f)
-                print("Demo saved")
+            raw_data_count += 1
+            if (torch.Tensor(robot_state.joint_positions) - old_robot_state[0]).abs().max() > 0.02 \
+                or abs(gripper_state.width - old_robot_state[1]) > 0.01:
+                if self.save_file:
+                    with open("demo.pkl", "ab") as f:
+                        record_obj.update(dict(
+                            robot_state=robot_state, gripper_state=gripper_state, 
+                            image=camera_image.astype(np.uint8) if camera_image.shape[2] == 3 else camera_image.astype(np.float32), 
+                            image_timestamp=stamp,
+                            # desired_eef_pos=eef_pos, desired_eef_quat=eef_quat, # 
+                        ))
+                        pickle.dump(record_obj, f)
+                        file_count += 1
+                        print("robot", robot_state.timestamp)
+                old_robot_state = (torch.Tensor(robot_state.joint_positions), gripper_state.width)
+        print("Demo count", file_count, "Raw data count", raw_data_count)
         
     def _camera_listener(self):
         old_timestampe = None
         while True:
-            if not self.read_image_lock:
-                image, timestamp = self.camera.read_once()
-                if timestamp != old_timestampe:
-                    self._image_buffer.append((image, timestamp))
-                    cv2.imshow(
-                        f"Video", 
-                        cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_RGB2BGR)
-                    )
-                    cv2.waitKey(25)
-                    old_timestampe = timestamp
-            # else:
-            #     time.sleep(0.1)
+            if not self.start_or_pause:
+                time.sleep(0.1)
+                continue
+            image, timestamp = self.camera.read_once()
+            if timestamp != old_timestampe:
+                self._image_buffer.append((image, timestamp))
+                cv2.imshow(
+                    f"Video", cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_RGB2BGR)
+                )
+                cv2.waitKey(25)
+                old_timestampe = timestamp
     
     def _joystick_listener(self):
+        dx, dy, dz = 0., 0., 0.
+        scale = 0.01
         while True:
             events = pygame.event.get([pygame.JOYAXISMOTION, pygame.JOYBUTTONDOWN, pygame.JOYHATMOTION])
-            dx, dy, dz = 0., 0., 0.
             gripper_toggle = False
             for event in events:
                 if event.type == pygame.JOYAXISMOTION:
-                    cur_value = np.sign(event.value) if abs(event.value) > 0.5 else 0
+                    cur_value = event.value
                     if event.axis == 3:
-                        if event.value > 0:
-                            dy = max(dy, 0.01 * cur_value)
-                        else:
-                            dy = min(dy, 0.01 * cur_value)
+                        dy = scale * cur_value
+                        # if event.value > 0:
+                        #     dy = max(dy, 0.01 * cur_value)
+                        # else:
+                        #     dy = min(dy, 0.01 * cur_value)
                     elif event.axis == 4:
-                        if event.value > 0:
-                            dx = max(dx, 0.01 * cur_value)
-                        else:
-                            dx = min(dx, 0.01 * cur_value)
+                        dx = scale * cur_value
+                        # if event.value > 0:
+                        #     dx = max(dx, 0.01 * cur_value)
+                        # else:
+                        #     dx = min(dx, 0.01 * cur_value)
                     elif event.axis == 1:
-                        if event.value > 0:
-                            dz = min(dz, -0.01 * cur_value)
-                        else:
-                            dz = max(dz, -0.01 * cur_value)
+                        dz = -scale * cur_value
+                        # if event.value > 0:
+                        #     dz = min(dz, -0.01 * cur_value)
+                        # else:
+                        #     dz = max(dz, -0.01 * cur_value)
                 elif event.type == pygame.JOYBUTTONDOWN:
                     if event.button == 4:
                         # if not gripper_state.is_moving:
                         gripper_toggle = True
+                    elif event.button == 0: #A
+                        self.start_or_pause = not self.start_or_pause
+                        print("Current status:", "start" if self.start_or_pause else "pause")
+                    elif event.button == 1: #B
+                        self.stopped = True
+                        print("Stopped")
             if gripper_toggle:
                 try:
                     self._command_queue.put({"type": "gripper_toggle"}, block=False)
                 except queue.Full:
                     pass
                 # self._command_queue.join()
-            elif abs(dx) > 0 or abs(dy) > 0 or abs(dz) > 0:
+            elif abs(dx) > scale * 0.01 or abs(dy) > scale * 0.01 or abs(dz) > scale * 0.01:
                 delta_pos = torch.Tensor([dx, dy, dz])
                 try:
                     self._command_queue.put({"type": "eef", "delta_pos": delta_pos}, block=False)
@@ -250,5 +284,5 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python examples/5_collect_demo.py [ip]")
     ip = sys.argv[1]
-    demo_controller = DemoCollector(ip_address=ip)
+    demo_controller = DemoCollector(ip_address=ip, save_file=True)
     demo_controller.run()
